7b8881aaa7b008b22da383e80893054c17129479
Éverton Toffanetto <evertondgn@hotmail.com>
2021-09-08 07:55:57 -0300

    refactor: remove files

--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -1,4 +1,1 @@
-export { default as ScreenRotation } from 'components/Icons/ScreenRotation'
 export { default as Checkbox } from 'components/Icons/Checkbox'
-export { default as PercentArrowTop } from 'components/Icons/PercentArrowTop'
-export { default as First } from 'components/Icons/First'
